b83bd78abbb4a1da3f5b0fe114a8ee2e2f3e05d9
Steve Parsons <sparsons@us.ibm.com>
2016-08-03 15:53:01 -0700

    For Release 3.1

--- src/Constants.java
+++ src/Constants.java
@@ -6,7 +6,7 @@ package com.ibm.mqmft.exit.fileordering;
 /**
  * © Copyright IBM Corporation 2010, 2016
  * 2010: Original release, IBM-internal
- * 2016: Version 3.0, external
+ * 2016: Release 3.1, external
  * author: Steve Parsons (sparsons@us.ibm.com, sparsons@sparsons.net)
  *
  */
