18d0b843b141bf28c44e5c3450e3fd2a446e6c27
Steve Parsons <sparsons@us.ibm.com>
2016-08-03 14:57:04 -0700

    for Release v3.0

--- javascripts/dlcounter.js
+++ javascripts/dlcounter.js
@@ -1,3 +1,5 @@
+// see https://github.com/Somsubhra/github-release-stats
+
 // Display the stats
 function showStats(data) {
 
@@ -24,66 +26,23 @@ function showStats(data) {
         var totalDownloadCount = 0;
 
         $.each(data, function(index, item) {
-           // var releaseTag = item.tag_name;
-           // var releaseURL = item.html_url;
             var releaseAssets = item.assets;
             var hasAssets = releaseAssets.length != 0;
-           // var releaseAuthor = item.author;
-           // var publishDate = item.published_at.split("T")[0];
-
-          //  if(latest) {
-          //      html += "<div class='row release latest-release'>" +
-          //          "<h2><a href='" + releaseURL + "' target='_blank'>" +
-          //          "<span class='glyphicon glyphicon-tag'></span>&nbsp&nbsp" +
-          //          "Latest Release: " + releaseTag +
-          //          "</a></h2><hr class='latest-release-hr'>";
-          //      latest = false;
-          //  } else {
-          //      html += "<div class='row release'>" +
-          //          "<h4><a href='" + releaseURL + "' target='_blank'>" +
-          //          "<span class='glyphicon glyphicon-tag'></span>&nbsp&nbsp" +
-          //          releaseTag +
-          //          "</a></h4><hr class='release-hr'>";
-          //  }
-
-          //  html += "<h4><span class='glyphicon glyphicon-info-sign'></span>&nbsp&nbsp" +
-          //      "Release Info:</h4>";
-
-          //  html += "<ul>";
-
-          //  html += "<li><span class='glyphicon glyphicon-user'></span>&nbsp&nbspRelease Author: " +
-          //      "<a href='" + releaseAuthor.html_url + "'>" + releaseAuthor.login  +"</a><br></li>";
-
-          //  html += "<li><span class='glyphicon glyphicon-calendar'></span>&nbsp&nbspPublished on: " +
-          //      publishDate + "</li>";
-
-          //  html += "</ul>";
         
             if(hasAssets) {
-         //       html += "<h4><span class='glyphicon glyphicon-download'></span>" +
-         //           "&nbsp&nbspDownload Info: </h4>";
-
-         //       html += "<ul>";
+ 
                 $.each(releaseAssets, function(index, asset) {
-         //           var assetSize = (asset.size / 1048576.0).toFixed(2);
-         //           var lastUpdate = asset.updated_at.split("T")[0];
-         //           html += "<li>" + asset.name + " (" + assetSize + " MiB) - Downloaded " +
-         //               asset.download_count + " times.<br><i>Last updated on " + lastUpdate + "</i></li>";
                     totalDownloadCount += asset.download_count;
                 });
-         //       html += "</ul>";
+         
             }
-         //   html += "</div>";
+         
         });
 
         if(totalDownloadCount > 0) {
             totalDownloadCount = totalDownloadCount.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,');
             var totalHTML = "<div class='row total-downloads'>";
-         //   totalHTML += "<h2><span class='glyphicon glyphicon-download'></span>" +
-         //       "&nbsp&nbspTotal Downloads</h2> ";
-         //   totalHTML += "<span>" + totalDownloadCount + "</span>";
-			totalHTML += "&nbsp&nbspTotal downloadsZZZZZZZZ: " + totalDownloadCount + "</div>";
-         //   totalHTML += "</div>";
+			totalHTML += "&nbsp&nbspTotal downloads: " + totalDownloadCount + "</div>";
             html = totalHTML + html;
         }
 
@@ -93,14 +52,12 @@ function showStats(data) {
     var resultDiv = $("#stats-result");
     resultDiv.hide();
     resultDiv.html(html);
-  //  $("#loader-gif").hide();
     resultDiv.slideDown();
 }
 
-// Callback function for getting release stats
+// function for getting release stats
 function getStats() {
     var url = "https://api.github.com/repos/ibm-messaging/mq-mft-file-ordering/releases";
-//	var url = "https://api.github.com/repos/ibm-messaging/mq-mft-file-ordering/releases/latest";
     $.getJSON(url, showStats).fail(showStats);
 }
 
