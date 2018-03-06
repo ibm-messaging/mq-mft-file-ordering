22e01e5ebc4b233b0d47b274cbf250fc71dd89d2
Steve Parsons <sparsons@us.ibm.com>
2018-03-05 22:20:20 -0800

    For Release 3.3

--- src/SortingChoice.java
+++ src/SortingChoice.java
@@ -3,6 +3,7 @@ package com.ibm.mqmft.exit.fileordering;
  * © Copyright IBM Corporation 2010, 2016
  * 2010: Original release, IBM-internal
  * 2016: Release 3.1, external
+ * 2018: Release 3.3, external
  * author: Steve Parsons (sparsons@us.ibm.com, sparsons@sparsons.net)
  *
  */
